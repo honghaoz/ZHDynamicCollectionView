89e964a3c57d2756b3bea9fe6137f2808b3a87f1
Honghao Zhang <zhh358@gmail.com>
2015-01-01 23:28:43 -0500

    Added classes

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-ZHDynamicCollectionView
-=======================
+#ZHDynamicCollectionView
 
-Maintain a set of offscreen cells and use them to help calculating dynamic cell size in sizeForItemAtIndexPath
+## Why
+For dynamic collection view cell, in sizeForItemAtIndexPath, we need to 
+initialize an off screen cell and use it to calculate cell's size.
+
+Normally, we will create an dictionary to maintain off screen cells, like this:
+
+In collectionView controller:
+
+```
+var offscreenCells = Dictionary<String, UICollectionViewCell>()
+```
+
+In 
+`collectionView:layout:sizeForItemAtIndexPath:`
+
+```
+var cell: MyCollectionViewCell? = collectionVC.offscreenCells[reuseIdentifier] as? MyCollectionViewCell
+if cell == nil {
+	cell = NSBundle.mainBundle().loadNibNamed("MyCollectionViewCell", owner: nil, options: nil)[0] as? MyCollectionViewCell
+	collectionVC.offscreenCells[reuseIdentifier] = cell
+}
+```
+
+This kind of code is too trival.
+
+## How to use
+
+Register cells as usual:
+
+```
+collectionView.registerClass(MyCollectionViewCell.self, forCellWithReuseIdentifier: kCellResueIdentifier)
+```
+or
+
+```
+var cellNib = UINib(nibName: "MyCollectionViewCell", bundle: nil)
+collectionView.registerNib(cellNib, forCellWithReuseIdentifier: kCellResueIdentifier)
+```
+
+In `collectionView:layout:sizeForItemAtIndexPath:`
+
+```
+var helperCell = collectionView.dequeueReusableOffScreenCellWithReuseIdentifier(kCellResueIdentifier) as MyCollectionViewCell
+
+var size = helperCell.contentView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+return size
+```
+
+Only One new API: 
+
+`- dequeueReusableOffScreenCellWithReuseIdentifier(identifier: String) -> UICollectionViewCell?`
---
+++ ZHDynamicCollectionView.swift
@@ -0,0 +1,49 @@
+//
+//  ZHDynamicCollectionView.swift
+//
+//  Created by Honghao on 1/1/15.
+//  Copyright (c) 2015 Honghao. All rights reserved.
+//
+
+import UIKit
+
+class ZHDynamicCollectionView: UICollectionView {
+    // A dictionary of offscreen cells that are used within the sizeForItemAtIndexPath method to handle the size calculations. These are never drawn onscreen. The dictionary is in the format:
+    // { NSString *reuseIdentifier : UICollectionViewCell *offscreenCell, ... }
+    private var offscreenCells = Dictionary<String, UICollectionViewCell>()
+    private var registeredCellNibs = Dictionary<String, UINib>()
+    private var registeredCellClasses = Dictionary<String, UICollectionViewCell.Type>()
+    
+    override func registerClass(cellClass: AnyClass?, forCellWithReuseIdentifier identifier: String) {
+        super.registerClass(cellClass, forCellWithReuseIdentifier: identifier)
+        registeredCellClasses[identifier] = cellClass as UICollectionViewCell.Type!
+    }
+    
+    override func registerNib(nib: UINib?, forCellWithReuseIdentifier identifier: String) {
+        super.registerNib(nib, forCellWithReuseIdentifier: identifier)
+        registeredCellNibs[identifier] = nib
+    }
+    
+    /**
+    Returns a reusable collection cell object located by its identifier.
+    This collection cell is not showing on screen, it's useful for calculating dynamic cell size
+    :param: identifier A string identifying the cell object to be reused. This parameter must not be nil.
+    
+    :returns: UICollectionViewCell?
+    */
+    func dequeueReusableOffScreenCellWithReuseIdentifier(identifier: String) -> UICollectionViewCell? {
+        var cell: UICollectionViewCell? = offscreenCells[identifier]
+        if cell == nil {
+            if registeredCellNibs.has(identifier) {
+                let cellNib: UINib = registeredCellNibs[identifier]! as UINib
+                cell = cellNib.instantiateWithOwner(nil, options: nil)[0] as? UICollectionViewCell
+            } else if registeredCellClasses.has(identifier) {
+                let cellClass = registeredCellClasses[identifier] as UICollectionViewCell.Type!
+                cell = cellClass()
+            } else {
+                assertionFailure("\(identifier) is not registered in \(self)")
+            }
+        }
+        return cell
+    }
+}
